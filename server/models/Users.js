fe58dec02f61a1a810cb37538da7f6409f580aec
farid555 <farid555a@gmail.com>
2022-06-07 21:39:50 +0300

    created models

---
+++ server/models/Users.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+UserSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+UserSchema.set("toJSON", {
+  virtuals: true,
+});
+
+exports.User = mongoose.model("User", UserSchema);
