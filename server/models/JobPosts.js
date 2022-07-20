5c38b7cf419aa6aab659814b095f3385cd040bf4
Balar <balar9426@gmail.com>
2022-07-20 21:30:16 +0530

    ADD CODES IN SELECTOPTION FILE

--- server/models/JobPosts.js
+++ server/models/JobPosts.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { skillOptions, certificationOptions } = require("../utils/SelectOptions");
 
 const JobPostSchema = new mongoose.Schema({
   position: {
@@ -98,10 +99,40 @@ const JobPost = mongoose.model("Job_Posts", JobPostSchema);
 
 JobPostSchema.pre(/^save|findOneAndUpdate$/, true, async function (next, done) {
   try {
-    this.skillCode = ":A:";
-    next();
-  }
-  catch (err) { done(err); next(); }
+        
+    const skillList = this.skills.toString().split(',');
+    console.log(skillList);
+
+    const matchSkill = skillList.map(i=>skillOptions.find(j=>i === j.value))
+    // [ {value: 'Teamwork', label: 'Teamwork', code: 'A'},{value: 'Legal', label: 'Legal', code: 'B'}]
+    console.log("matchSkill==>",matchSkill);
+    console.log("matchSkill==>",matchSkill.length);
+    if(matchSkill.length>0){
+        console.log("log inside" );
+        const temp = matchSkill.sort((a,b)=> a.code.length - b.code.length || a.code.charCodeAt(0) - b.code.charCodeAt(0)).map(i=>`:${i.code}:`).join("")
+        console.log("temp",temp);
+
+        this.skillCode =temp;
+    }
+    const certificationList = this.certifications.toString().split(',');
+    console.log(certificationList);
+
+    const matchcertifications = certificationList.map(i=>certificationOptions.find(j=>i === j.value))
+    // [ {value: 'Teamwork', label: 'Teamwork', code: 'A'},{value: 'Legal', label: 'Legal', code: 'B'}]
+    console.log("matchSkill==>",matchcertifications);
+    console.log("matchSkill==>",matchcertifications.length);
+    if(matchcertifications.length>0){
+        console.log("log inside" );
+        const temp = matchcertifications.sort((a,b)=> a.code.length - b.code.length || a.code.charCodeAt(0) - b.code.charCodeAt(0)).map(i=>`:${i.code}:`).join("")
+        console.log("temp",temp);
+
+        this.certificationCode =temp;
+    }
+  done();
+  next();
+}
+catch (err) {
+    console.log(err); done(err); next(); }
 });
 
 module.exports = JobPost;
